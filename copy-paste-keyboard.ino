e7d49566ee9eba62bd120937f8742f0565a35d60
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-06-02 18:32:44 +0700

    Update copy-paste-keyboard.ino
    Enhance with LCD

--- copy-paste-keyboard.ino
+++ copy-paste-keyboard.ino
@@ -1,9 +1,21 @@
+#include <LiquidCrystal_I2C.h>
 #define COPY_BUTTON 3
 #define PASTE_BUTTON 2
 #define LED_1 13
 #define LED_2 12
+#define COPY_STR "Ctrl + C"
+#define PASTE_STR "Ctrl + V"
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Arduino Trigger");
+  lcd.setCursor(0, 1);
+  lcd.print("Copy & Paste");
+
   Serial.begin(9600);
   pinMode(COPY_BUTTON, INPUT_PULLUP);
   pinMode(PASTE_BUTTON, INPUT_PULLUP);
@@ -11,20 +23,20 @@ void setup() {
   pinMode(LED_2, OUTPUT);
 }
 
+
+
 void loop() {
-  if (digitalRead(COPY_BUTTON) == 0) {
-    Serial.println("COPY");
-    digitalWrite(LED_1, HIGH);
+  trigger(COPY_BUTTON, COPY_STR, LED_1);
+  trigger(PASTE_BUTTON, PASTE_STR, LED_2);
+}
+
+ void trigger(int button, String msg, int led) {
+  if (digitalRead(button) == 0) {
+    Serial.println(msg);
+    lcd.clear();
+    lcd.print(msg);
+    digitalWrite(led, HIGH);
     delay(200);
-    digitalWrite(LED_1, LOW);
-
-  }
-
-  if (digitalRead(PASTE_BUTTON) == 0) {
-    Serial.println("PASTE");
-    digitalWrite(LED_2, HIGH);
-    delay(200);
-    digitalWrite(LED_2, LOW);
-
+    digitalWrite(led, LOW);
   }
 }
